e0af301818298ca79eeccfa08d1d4c2cbb9ff374
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-06-16 13:25:33 +0300

    assingment completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+     cats.push("Ralph");
+
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+    
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    
+}
+function appendCat(Broom) {
+    return[...cats,'Broom'];
+    
+}
+function prependCat(name) {
+    const newCats=[name,...cats];
+    return newCats;
+    
+}
+function removeLastCat(){ 
+return cats.slice(0, -1);
+    
+}
+function removeFirstCat() {
+ return cats.slice(1);  
+}
